92290dd471349e55b2e7d8533c3500829f89ab2c
barsanges <barsanges@users.noreply.github.com>
2021-11-03 21:19:46 +0100

    Add the option to provide the inventory as a CSV-like string.

--- src/Main.hs
+++ src/Main.hs
@@ -16,34 +16,44 @@ import Algorithm
 data Usage = Ask | DryRun | Yes | Variants
   deriving Eq
 
+-- | Format of the inventory: CSV file or CSV-like string.
+data Input = Fin String | Str String
+
 -- | Command line arguments.
-data Args = Args { totalCost :: Double, fin :: String, usage :: Usage, comma :: Bool }
+data Args = Args { totalCost :: Double
+                 , input :: Input
+                 , usage :: Usage
+                 , comma :: Bool
+                 }
 
 argsParser :: Parser Args
 argsParser = Args
   <$> ( argument auto
         ( metavar "COST"
         <> help "Cost of the letter in euros" ))
-  <*> ( strOption
-        ( long "inventory"
-        <> short 'i'
-        <> metavar "FNAME"
-        -- The default is set manually in the help text because
-        -- 'showDefault' gives a weird spacing:
-        <> help "CSV file containing a stamps inventory (unitary\
-                \ prices and available quantities); default: \
-                \'./inventory.csv'"
-        <> value "./inventory.csv" ))
+  <*> ( ( Fin <$> strOption
+          ( long "file"
+          <> short 'f'
+          <> metavar "FNAME"
+          <> help "CSV file containing a stamps inventory (unitary\
+                  \ prices and available quantities)" ))
+      <|> ( Str <$> strOption
+            ( long "string"
+            <> short 's'
+            <> metavar "STRING"
+            <> help "CSV-like string containing a stamps inventory (unitary\
+                    \ prices and available quantities)" )))
   <*> ( ( flag Ask DryRun
           ( long "dry-run"
           <> help "If this option is used, the inventory file is not\
-                  \ updated" ))
+                  \ updated (useless if the option '--string' is used)" ))
       <|> ( flag' Yes
             ( long "yes"
             <> short 'y'
             <> help "If this option is used, the program does not\
                     \ ask for confirmation before updating the\
-                    \ inventory file" ))
+                    \ inventory file  (useless if the option\
+                    \ '--string' is used)" ))
       <|> ( flag' Variants
             ( long "variants"
             <> help "If this option is used, the program returns\
@@ -66,22 +76,24 @@ args = info (argsParser <**> helper)
 main :: IO ()
 main = do
   cli <- execParser args
-  maybeInventory <- readInventoryFile (comma cli) (fin cli)
+  maybeInventory <- case input cli of
+      Fin fin -> readInventoryFile (comma cli) fin
+      Str s -> pure (readInventoryString (comma cli) s)
   case maybeInventory of
     Left err -> putStrLn err
     Right inventory -> case optimum (totalCost cli) inventory of
       Left msg -> putStrLn msg
       Right opt -> do
         (putStrLn . reprSolution) opt
-        case (usage cli) of
-          Ask -> confirm "Do you want to update the inventory (Y/n)?" (update opt)
-          Yes -> (update opt)
-          Variants -> do
+        case (usage cli, input cli) of
+          (Ask, Fin f) -> confirm "Do you want to update the inventory (Y/n)?" (update f opt)
+          (Yes, Fin f) -> (update f opt)
+          (Variants, _) -> do
             putStrLn "Variants:"
             (putStrLn . reprVariants) (variants opt)
-          DryRun -> return ()
+          _ -> return ()
       where
-        update x = writeInventoryFile (comma cli) (fin cli) (resultingInventory x)
+        update f x = writeInventoryFile (comma cli) f (resultingInventory x)
 
 -- | Ask for confirmation before performing an action.
 confirm :: String -> IO () -> IO ()
