8d1a15f51e728528b1dcb848558aac4042cc9a4e
barsanges <barsanges@users.noreply.github.com>
2025-02-24 21:43:16 +0100

    Rewrite the 'solve' function in order to speed it up.

--- src/Algorithm.hs
+++ src/Algorithm.hs
@@ -32,27 +32,52 @@ withinRange mn low up inventory
   | low < 0 = Left "The minimum value should be a positive float!"
   | up < low = Left "The maximum value should be greater than the minimum value!"
   | any (\ n -> n <= 0) mn = Left "The maximal number of stamps should be strictly positive!"
-  | otherwise = if Set.null res
+  | otherwise = if null res
                 then Left "The problem is infeasible!"
-                else Right (Seq.sort (Seq.fromList . toList $ res))
+                else Right (Seq.sort (Seq.fromList res))
     where
-      res = solve mn low up inventory
+      tmp = solve mn (low - 1e-9) (up + 1e-9) (collectionToList inventory)
+      res = fmap (foldr add (emptyLike inventory)) tmp
 
--- | The function that actually search the sets of stamps whose total value lies
--- within the given range. Do not perform any check regarding the validity of
--- the inputs.
-solve :: Maybe Int -> Double -> Double -> Collection -> Set Collection
-solve mn low up inventory = res
+-- | The function that actually search the sets of stamps whose total
+-- value lies within the given range. Do not perform any check
+-- regarding the validity of the inputs.
+solve :: Maybe Int -> Double -> Double -> [StampSet] -> [[StampSet]]
+solve mn low up inventory = filterBelow low (filter (\ x -> fst x /= 0) $ findAbove mn up inventory)
+
+-- | Filter the pairs whose first value is below a given threshold.
+filterBelow :: Ord a => a -> [(a, [b])] -> [[b]]
+filterBelow threshold = go
   where
-    (_, res) = crease go (Set.singleton (emptyLike inventory), Set.empty) inventory
+    go [] = []
+    go ((cost, xs):yss) | cost >= threshold = xs:(go yss)
+                        | otherwise = go yss
 
-    go :: StampSet
-       -> (Set Collection, Set Collection)
-       -> (Set Collection, Set Collection)
-    go s (partial, complete) = (partial', Set.union complete complete')
+-- | Find the combinations of stamps whose total value is above a
+-- given threshold. Each combination should contain less than a given
+-- number of stamps.
+findAbove :: Maybe Int -> Double -> [StampSet] -> [(Double, [StampSet])]
+findAbove _ _ [] = [(0, [])]
+findAbove (Just 0) _ _ = [(0, [])]
+findAbove mn up (x:ys) | up < 0 = [(0, [])]
+                       | otherwise = concatMap go [0..imax]
+  where
+    imax :: Int
+    imax = case mn of
+             Nothing -> min (floor (up / (price x))) (quantity x)
+             Just n -> min n (min (floor (up / (price x))) (quantity x))
+    go :: Int -> [(Double, [StampSet])]
+    go i = [ (cost + current, (changeQuantity x i):zs)
+           | (cost, zs) <- findAbove mn' up' ys ]
       where
-        cols = Set.unions (Set.map (mkCollectionRange s) partial)
-        (partial', complete') = sieve mn low up cols
+        mn' = mn `mminus` i
+        current = (price x) * (fromIntegral i)
+        up' = up - current
+
+-- | Substract a value to a Maybe value.
+mminus :: Num a => Maybe a -> a -> Maybe a
+mminus Nothing _ = Nothing
+mminus (Just x) y = Just (x - y)
 
 -- | Create all collections resulting from the union of `col` and the
 -- elements of `mkRange s`.
